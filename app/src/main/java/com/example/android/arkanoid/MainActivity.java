6c87089bf742bc2fb8a265b2db132e2bd912ea27
Alessio Micunco <ale.micunco@gmail.com>
2021-12-12 00:01:10 +0100

    MainActivity: safely retrieve custom level params

--- app/src/main/java/com/example/android/arkanoid/MainActivity.java
+++ app/src/main/java/com/example/android/arkanoid/MainActivity.java
@@ -19,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
         // imposta l'orientamento dello schermo
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        String custom_level = getIntent().getExtras().getString("custom_level");
+        String custom_level = "";
+        if (getIntent().getExtras() != null) {
+            custom_level = getIntent().getExtras().getString("custom_level");
+        }
         // crea un nuovo gioco
         game = new Game(this, 3, 0, custom_level);
         setContentView(game);
