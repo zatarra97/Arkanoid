2b2cedd96c64b8214dc0cb208b17a4e1d0fc84ce
Raffaele Caputi <caputiraffaele@gmail.com>
2022-02-15 09:38:40 +0100

    inserimento funzioni cambio lingua

--- app/src/main/java/com/example/android/arkanoid/SettingsPage.java
+++ app/src/main/java/com/example/android/arkanoid/SettingsPage.java
@@ -1,28 +1,62 @@
 package com.example.android.arkanoid;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.support.design.chip.ChipGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class SettingsPage extends AppCompatActivity {
-    Switch mySwitch = null;
+
+    private Button lbtn;
+    private TextView language;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocate();
         setContentView(R.layout.activity_settings_page);
 
         //per far comparire la freccia in alto a sinistra
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        lbtn = findViewById(R.id.btnLang);
+        language = findViewById(R.id.tvlanguage);
+
+        language.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                        showChangeLang();
+            }
+        });
+
+        lbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showChangeLang();
+            }
+        });
+
+
+
+
+
     }
 
-    //viene chiamato quando si preme il tasto indietro in altro a sinistra
+    //viene chiamato quando si preme il tasto indietro in alto a sinistra
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -48,5 +82,52 @@ public class SettingsPage extends AppCompatActivity {
         finish();
     }
 
+    private void setLocate(String lang){
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+
+        getBaseContext().getResources().updateConfiguration(
+                config, getBaseContext().getResources().getDisplayMetrics());
+
+        SharedPreferences.Editor editor= getPreferences(Context.MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    private void loadLocate(){
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        String language = sharedPreferences.getString("My_Lang", "");
+        if(language!=""){
+            setLocate(language);
+        }
+    }
+
+    private void showChangeLang(){
+        String listItems[]={"English","Italiano"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setTitle(R.string.selectLangTitle);
+
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        setLocate("en");
+                        recreate();
+                        break;
+                    case 1:
+                        setLocate("it");
+                        recreate();
+                        break;
+                }
+                dialog.dismiss();
+            }
+        });
+        DialogInterface mDialog = mBuilder.create();
+        mBuilder.show();
+
+    }
 
 }
